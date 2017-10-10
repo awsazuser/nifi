19d9b9edbe9e224fd6f58ea82db631020ff7dc17
awsazuser <awsazuser@gmail.com>
2017-10-10 01:39:34 +0000

    First commit

--- startnifi.sh
+++ startnifi.sh
@@ -5,5 +5,7 @@ sed -i -r "s|^nifi.zookeeper.connect.string=.*$|nifi.zookeeper.connect.string=zo
 sed -i -r "s|^nifi.cluster.is.node=.*$|nifi.cluster.is.node=true|g" ${NIFI_HOME}/conf/nifi.properties
 sed -i -r "s|^nifi.cluster.node.address=.*$|nifi.cluster.node.address=`hostname`|g" ${NIFI_HOME}/conf/nifi.properties
 sed -i -r "s|^nifi.cluster.node.protocol.port=.*$|nifi.cluster.node.protocol.port=8081|g" ${NIFI_HOME}/conf/nifi.properties
+sed -i -r "s|^nifi.cluster.node.connection.timeout=.*$|nifi.cluster.node.connection.timeout = 30 sec|g" ${NIFI_HOME}/conf/nifi.properties
+sed -i -r "s|^nifi.cluster.node.read.timeout=.*$|nifi.cluster.node.read.timeout = 30 sec|g" ${NIFI_HOME}/conf/nifi.properties
 
 ${NIFI_HOME}/bin/nifi.sh $1
